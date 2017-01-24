59bb969e497ec0a4d689993f3217dc97c3a59b71
jnoortheen <jnoortheen@gmail.com>
2017-01-24 13:06:56 +0530

    feat: added index page server

---
+++ bin/server.dart
@@ -0,0 +1,16 @@
+import 'package:redstone/redstone.dart' as app;
+
+// JSON services
+@app.Install(urlPrefix: "/services")
+import "package:dart_mobile_catalog/server/json_service.dart";
+
+/// return the home page
+@app.Route("/")
+index() {
+  return "index.html";
+}
+
+main() {
+  app.setupConsoleLog();
+  app.start();
+}
